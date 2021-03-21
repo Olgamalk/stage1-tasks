99c5a3669f1d1e885fd79af62d6d8222b358eaab
Olga Malkovich <olga.malkovich@gmail.com>
2021-03-21 20:27:50 +0300

    fix: add some code

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,34 +1,17 @@
 const COLLECTION = document.querySelectorAll('.piano-key');
-
-// COLLECTION.forEach((elem) => {
-//     elem.addEventListener('mousedown', (event) => {
-//         event.target.classList.add('active');
-//     })
-//     elem.addEventListener('mouseup', (event) => {
-//         event.target.classList.remove('active');
-//     })
-// })
-
 const PIANO = document.getElementById("piano");
 
-// PIANO.addEventListener('mousedown', (event) => {
-//             event.target.classList.add('active');
-//         })
-// PIANO.addEventListener('mouseup', (event) => {
-//             event.target.classList.remove('active');
-//         })
-
 const startSound = (event) => {
-    event.target.classList.add("active");
+    event.target.classList.add("piano-key-active");
 }
 
 const stopSound = (event) => {
-    event.target.classList.remove("active");
+    event.target.classList.remove("piano-key-active");
 }
 
 const startCorrespondOver = (event) => {
     if (event.target.classList.contains("piano-key")) {
-        event.target.classList.add("active");
+        event.target.classList.add("piano-key-active");
     }
 
     COLLECTION.forEach((elem) => {
@@ -39,11 +22,51 @@ const startCorrespondOver = (event) => {
 
 const stopCorrespondOver  = () => {
     COLLECTION.forEach((elem) => {
-        elem.classList.remove("active");
+        elem.classList.remove("piano-key-active");
         elem.removeEventListener("mouseover", startSound)
         elem.removeEventListener("mouseout", stopSound)
     });
 }
 
-PIANO.addEventListener("mousedown", startCorrespondOver, false)
-PIANO.addEventListener("mouseup", stopCorrespondOver)
+PIANO.addEventListener("mousedown", startCorrespondOver, false);
+PIANO.addEventListener("mouseup", stopCorrespondOver);
+
+PIANO.addEventListener("mousedown", (event) => {
+    if(event.target.classList.contains('piano-key')) {
+      const note = event.target.dataset.note;
+      const src = `assets/audio/${note}.mp3`;
+      playAudio(src);
+    }   
+  });
+
+function playAudio (src) {
+    const audio = new Audio();
+    audio.src = src;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const button = document.querySelectorAll(".btn");
+ button.addEventListener("click", (e) => {
+     const txt = e.target.innerText;
+     if (txt === 'Notes') {
+         e.target.innerText = 'Letter';
+     }
+ })
+
+
+
+
+// При клике на кнопку добавляйте ей btn-active, а со второй убирайте и потом через for(let elem of pianoKeys){
+//     elem.classList.add("название класса")} так сразу будет добавляться класс ко всем piano-key
+
+const fullScreen = document.querySelector(".fullscreen");
+fullScreen.addEventListener('click', (event) => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+});
--- virtual-piano/script1.js
+++ virtual-piano/script1.js
@@ -1,6 +1,17 @@
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.piano-key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("piano-key-active");
+});
+
+window.addEventListener('keyup', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.piano-key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.play();
-
+    key.classList.remove("piano-key-active");
 });
